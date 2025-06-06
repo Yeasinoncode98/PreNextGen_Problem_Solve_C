bc19a581accb87d5e96b2bc6de9553946dc50c41
Yeasinoncode98 <devoncode98@gmail.com>
2025-06-06 19:32:11 +0600

    added

---
+++ problem11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1;
+    int n2;
+    int sum;
+
+    printf("Enter First integer: ");
+    scanf("%d", &n1);
+
+    printf("Enter Second Integer: ");
+    scanf("%d", &n2);
+
+    sum = (n1 * n2);
+
+    printf("Sum:%d\n", sum);
+
+    return 0;
+}
