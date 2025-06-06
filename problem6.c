305d255584c18ad0074cdded977f74aad8cc0570
Yeasinoncode98 <devoncode98@gmail.com>
2025-06-06 19:26:20 +0600

    a

---
+++ problem6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1 = 50;
+    float n2 = 40.50;
+    double n3 = 50.464664;
+
+    printf("%d\n", n1);
+    printf("%f\n", n2);
+    printf("%lf\n", n3);
+
+    return 0;
+}
