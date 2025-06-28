ed307b235a52d0ae27990278273f55b9bf7d2b66
Yeasin Arafat <devoncode98@gmail.com>
2025-06-29 04:00:32 +0600

    Update problem030.c

--- problem030.c
+++ problem030.c
@@ -1,28 +1,16 @@
-#include <stdio.h>
-int main()
-{
+#include<stdio.h>
+int main() {
     char ch;
-
-    printf("Enter a character: ");
     scanf("%c", &ch);
-
-    if ((ch >= 'a' && ch <= 'z')(ch >= 'A' && ch <= 'Z'))
-    {
-
-        if (ch == 'a' ch == 'A' ch == 'e' ch == 'E' ch == 'i' ch == 'I' ch == 'o' ch == 'O' ch == 'u' ch == 'U')
-        {
-
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U') {
             printf("%c is a vowel\n", ch);
         }
-        else
-        {
+        else {
             printf("%c is a consonant\n", ch);
         }
+    } else {
+        printf("%c is not an alphabet\n", ch);
     }
-    else
-    {
-        printf("%c is not an alphabetic character\n", ch);
-    }
-
     return 0;
 }
